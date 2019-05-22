dfb67969ccb97cdaa61f7d3c972b35812319c927
Armen <you@example.com>
2019-05-22 12:31:29 +0400

    update homework

--- Armen_Nersesyan/Homeworks/C++/11_05_19/vector.cpp
+++ Armen_Nersesyan/Homeworks/C++/11_05_19/vector.cpp
@@ -4,6 +4,7 @@ class Vector
 {	
 	int itssize;
 	int* itsarr;
+	int rezerv_size = 0;
 	public:
 	Vector(void){
 	}	
@@ -16,7 +17,7 @@ class Vector
 		itssize = chap;
 	}
 	void set_atindex(int index,int value){
-		std::cout<<"set_atindex funkciayi kanch"<<std::endl;
+	    //std::cout<<"set_atindex funkciayi kanch"<<std::endl;
 		itsarr[index] = value;
 	}
 	void pop_back(void){
@@ -39,15 +40,25 @@ class Vector
 	}
 	void push_back(int newelement){
 		std::cout<<"kanchvec push_back funkcian myvector obyekti vra"<<std::endl;
-		int* temp = new int[itssize + 1];
-		for(int i = 0; i < itssize +1; ++i){
-			temp[i] = itsarr[i];
-		}
-		temp[itssize] = newelement;
-		delete [] itsarr;
-		itsarr = temp;
-   		temp = NULL;
+		if(rezerv_size = 0){
+			rezerv_size  = 5;
+			int* temp = new int[itssize + rezerv_size];
+			for(int i = 0; i < itssize + 1;++i){
+				temp[i] = itsarr[i];
+			}
+			temp[itssize] = newelement;
+			for(int i = itssize + 1; i < itssize+ rezerv_size;++i){
+				temp[i] = 0;
+			}
+			delete [] itsarr;
+			itsarr = temp;
+			++itssize;
+			--rezerv_size;
+		}else{
+		itsarr[itssize] = newelement;
 	 	++itssize;	
+		--rezerv_size;
+		}
 	}
 	void swap(int index1, int index2){
 		if(index1 > itssize || index2 > itssize){
@@ -107,6 +118,42 @@ class Vector
         }
 
 	}
+	void front_push(int value){
+		int* temp = new int[itssize + 1];
+		temp[0] = value;
+		for(int i = 0, j = 1; i < itssize,j < itssize + 1;++i,++j ){
+			temp[j] = itsarr[i];
+		}
+		delete [] itsarr;
+		itsarr = temp;
+		temp = NULL;
+		++itssize;
+	
+	}
+	void front_pop(){
+		int* temp = new int[itssize - 1];
+		for(int i = 0,j = 1; i < itssize - 1,j < itssize;++i,++j){
+			temp[i] = itsarr[j];
+		}
+		delete [] itsarr;
+		itsarr = temp;
+		temp = NULL;
+		--itssize;
+	}
+	int vector_max(){
+	int max = itsarr[0];
+		for(int i = 1; i < itssize;++i){
+			if(max < itsarr[i]){
+				max = itsarr[i];
+			}
+		}
+		return max;
+	}
+	void clear(){
+		for(int i = 0; i < itssize;++i){
+			itsarr[i] = 0;
+		}
+	}
 	~Vector(void){
 		std::cout<<"desktruktori kanch delete masiv"<<std::endl;
 		delete [] itsarr;
@@ -122,17 +169,21 @@ int main(){
 	}
 	//myvector.set_atindex(0,10);
 	myvector.print();
-	//std::cout<<"size myvector-> "<<myvector.get_size()<<std::endl;
+	std::cout<<"size myvector-> "<<myvector.get_size()<<std::endl;
 	//myvector.pop_back();
 	//std::cout<<"size myvector-> "<<myvector.get_size()<<std::endl;
-	//myvector.push_back(50);
+	myvector.push_back(50);
 	//std::cout<<"size myvector-> "<<myvector.get_size()<<std::endl;
 	//myvector.insert_atindex(1,2,7777777);
     //std::cout<<"size myvector-> "<<myvector.get_size()<<std::endl;
 	//std::cout<<myvector.get_atindex(15)<<std::endl;
 	//myvector.swap(2,3);
 	//myvector.revers();
-	myvector.sort_nvazman();
+	//myvector.sort_nvazman();
+	//myvector.front_push(20);
+	//myvector.front_pop();
+	//myvector.clear();
 	myvector.print();
+	//std::cout<<myvector.vector_max()<<std::endl;
 	return 0;
 }
