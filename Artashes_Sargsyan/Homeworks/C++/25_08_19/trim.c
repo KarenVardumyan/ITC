8f9de226de174d5c222f8a26cb16a00f327bb1ef
KarenVardumyan <karenvardumyan94@gmail.com>
2019-08-25 18:37:54 +0400

    In progress

--- Artashes_Sargsyan/Homeworks/C++/25_08_19/trim.c
+++ Artashes_Sargsyan/Homeworks/C++/25_08_19/trim.c
@@ -10,13 +10,13 @@ typedef enum TrimMode {
 } TrimMode;
 
 char *trim(char *str, char *symbols, TrimMode mode, char *trailing_token);
-
+bool is_not_contain_char(char *str, char symbol);
 
 
 int main() {
 
     char *str = (char *) malloc(100);
-    str = strcpy(str, "           aaaaaaaaaaaaaaaaaaaaaaaaaaaaa    %@ ");
+    str = strcpy(str, "  %&%%%&&%&%&%&%&&% karen armen arto  %@ ");
 
 
     char *trailing_token = (char *) malloc(strlen(str));
@@ -24,7 +24,7 @@ int main() {
 
 
     printf("str : |%s|\naddres : %p\n", str,str);
-    str = trim(str, " ,\\#~!@#$%^&*()_\t;\"", FULL_TRIM, trailing_token);
+    str = trim(str, " %@&", START_TRIM, trailing_token);
     printf("\nstr : |%s|\naddres : %p\n", str, str);
 
     printf("\n\ntrailing token : |%s|\n", trailing_token);
@@ -37,25 +37,49 @@ int main() {
 
 
 /*
-* Trim the given string with the given symbols 
+* Trim the given string with the given symbols
 * str - input string
 * symbols - the symbols which should be removed
 * mode - the trim mode, can be FULL_TRIM, END_TRIM, START_TRIM
 * trailing_token - string containing trailing characters which were trimmed
 */
 char *trim(char *str, char *symbols, TrimMode mode, char *trailing_token) {
-    int sizeCut = 0;
-    switch (mode) {
-        case(FULL_TRIM):
-        for(int i = 0; i < strlen(str); ++i) {
-            for(int j = 0; j < strlen(symbols); ++j) {
-                if(str[i] == symbols[j]) {
-                    ++sizeCut;
-                }
-            }
+    int cutbegin = 0;
+    int cutend = 0;
+
+    for(int i = 0; i < strlen(str); ++i) {
+        if (is_not_contain_char(symbols, str[i])) {
+            break;
         }
+        cutbegin++;
+    }
+    printf("cutbegin = %d\n", cutbegin);
+
+    for(int i = strlen(str) - 1; i > 0; --i) {
+        printf("gfgfgfg\n");
+        if (is_not_contain_char(symbols, str[i])) {
+            break;
+        }
+        cutend++;
+    }
 
 
+    printf("cutend = %d\n", cutend);
+
+    switch (mode) {
+        case(START_TRIM):
+            memmove(str,
+                    str + cutbegin,
+                    cutbegin);
         break;
     }
+}
+
+bool is_not_contain_char(char *str, char symbol) {
+    for(int i = 0; i < strlen(str); ++i) {
+        if( *(str + i) == symbol) {
+            return false;
+        }
+    }
+    return true;
 }
