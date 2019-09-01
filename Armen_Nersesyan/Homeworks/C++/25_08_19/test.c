7d03850aa8eb9d13da6de68577598ba05d4a0fe7
KarenVardumyan <karenvardumyan94@gmail.com>
2019-09-01 17:42:59 +0400

    jsjdjfd

--- Armen_Nersesyan/Homeworks/C++/25_08_19/test.c
+++ Armen_Nersesyan/Homeworks/C++/25_08_19/test.c
@@ -3,22 +3,41 @@
 #include<string.h>
 #include<stdbool.h>
 
+#define SIZE 80
+bool is_equal(char *str1, char* str2);
 
 int main(int argc, char const *argv[]) {
 
-    char *text;
+    char* text1 =  (char*)malloc(SIZE);
+    text1 = strcpy(text1,"armenn");
 
-    text = (char *) malloc(100);
-    strcpy(text, "aaaaaaaaaaaaa");
+    char* text2 =  (char*)malloc(SIZE);
+    text2 = strcpy(text2,"anasun");
 
-
-    char *text1 = strdup(text);
-    printf("text 1 = |%s|\n", text1);
-
+     if(is_equal(text1,text2)) {
+         printf("EQUAL\n");
+     } else {
+         printf("NOT EQUAL\n");
+     }
 
     free(text1);
-    free(text);
+    free(text2);
 
     return 0;
 }
 
+bool is_equal(char *str1, char* str2) {
+    if(strlen(str1) != strlen(str2)){
+        return false;
+    }
+    for(int i = 0; i < strlen(str1); ++i){
+        if((int)str1[i] == (int)str2[i] ||
+           (int)str1[i] == (int)str2[i] + 32 ||
+           (int)str1[i] == (int)str2[i] - 32) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
