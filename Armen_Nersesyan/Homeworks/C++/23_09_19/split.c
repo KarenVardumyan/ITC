eca6f7bbb3d69491b841bce601bfea73e16d5444
Armen Nersisyan <armennersisyan95@gmail.com>
2019-09-26 17:08:24 +0400

    update split.c

--- Armen_Nersesyan/Homeworks/C++/23_09_19/split.c
+++ Armen_Nersesyan/Homeworks/C++/23_09_19/split.c
@@ -1,24 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include<stdbool.h>
+bool comparison(char* str1,char* str2);
 char**  split(char* str,char symbol, int* count);
-void destroy(char**);
 
 int main(int argc, char const *argv[]) {
 
     char symbol = ' ';
-    // scanf("%c",&symbol);
     int count = 0;
-    char *str = "karen armen vahe peto beno";
+    char *str = "karen armen valod babken aaaaaaa";
 
     char **result = split(str,symbol, &count);
-    // for (int i = 0; i < count; i++) {
-    //     printf("arr[%d] = %s\n", i, result[i]);
-    // }
+    for (int i = 0; i < count; i++) {
+         printf("arr[%d] = %s\n", i, result[i]);
+    }
 
 
     return 0;
+}
+bool comparison(char* str1,char* str2){
+    if(strlen(str1) == 0){
+        return true;
+    }
+    int size_string = 0;
+    if(strlen(str1) > strlen(str2)){
+        size_string = strlen(str2);
+    }else{
+        size_string = strlen(str1);
+    }
+    for(int i = 0; i < size_string; ++i){
+        if((int)str1[i] < (int)str2[i]){
+            return true;
+        }else if((int)str1[i] > (int)str2[i]){
+            return false;
+        }else{
+            ++i;
+            if( i >= size_string && strlen(str1) < strlen(str2)){
+                return true;
+            }
+        }
+    }
 }
 
 char**  split(char* str,char symbol, int* count) {
@@ -40,22 +62,20 @@ char**  split(char* str,char symbol, int* count) {
         if(str[i] == symbol || str[i] == '\0'){
             arr[index] = (char*)malloc(i - b);
             strncpy(arr[index], str + b, i - b);
-            printf("========> %s\n",arr[0]);
             b = i + 1;
             ++index;
         }
     }
-
+    arr[0] = "karen";
+    char* temp = NULL;
+    for(int start = 0; start < countsymbol; ++start){
+        for (int i = 0,j = 1; i < countsymbol,j < countsymbol; ++i, ++j){
+            if(comparison(arr[i],arr[j]) == false){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
     return arr;
 }
-
-
-
-
-
-
-
-
-
-
-
