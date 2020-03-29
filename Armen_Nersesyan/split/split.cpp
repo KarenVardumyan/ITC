da6b51fabe602f861e1d70191f9ef8b357bf76d5
Armen Nersisyan <armennersesyan95@gmail.com>
2020-03-29 21:10:05 +0400

    update split.cpp

--- Armen_Nersesyan/split/split.cpp
+++ Armen_Nersesyan/split/split.cpp
@@ -8,28 +8,26 @@ char** split(char* inputString,char token,int* count);
 int main(){
     char token = ' ';
     int count = 0;
-    char inputString[] ="gggg kdkdk dkdkkd kdkd jnjdefj";
+    char inputString[] ="gggg kjjnjnh fgf djkfndj djfn mdf md msdms smdms kdmsdf";
     if(inputString[0] == ' ' || inputString[strlen(inputString)-1] == 0){
-    trim(inputString);
+        trim(inputString);
     }
     char** output = split(inputString,token,&count);
     for (int i = 0; i <=count ; i++) {
-         printf("arr[%d] = %s\n", i, output[i]);
+        printf("arr[%d] = %s\n", i, output[i]);
     }
-
-    //for(int i = 0; i < count;++i){
-    //    std::cout<<*output[i]<<std::endl;
-    //}
+    free(output);
     return 0;
 }
 
 char** split(char* inputString,char token,int* count){
     int buffer = 0;
     int start = 0;
+    int end = 0;
     int index = 0;
     int countSplit = 0;
-    int size = strlen(inputString);
-    for(int i = 0; i < size; ++i){
+    int length = strlen(inputString);
+    for(int i = 0; i < length; ++i){
         if(inputString[i] == token){
             ++countSplit;
         }
@@ -37,26 +35,26 @@ char** split(char* inputString,char token,int* count){
             ++buffer;
         }
     }
-    if(buffer == size || size == 0){
+    if(buffer == length || length == 0){
         std::cout<<"empty string"<<std::endl;
         return 0;
     }
     *count = countSplit;
-    std::cout<<countSplit<<std::endl;
-    std::cout<<*count<<std::endl;
-    char** outputString = (char**) malloc(countSplit);
-    for(int i = 0; i < size; i++){
-        if(inputString[i] == token || inputString[i + 1] == '\0'){
-            outputString[index] = (char*) malloc(i - start);
-            memmove(outputString[index],inputString + start,i- start);
-            start = i + 1;
+    char** outputString = (char**)malloc(countSplit * sizeof(char*));
+    for(int i = 0; i < length; i++){
+        if(inputString[i] == token || inputString[i+1] == '\0') {
+            end = i;
+            outputString[index] = (char*) malloc(end- start + 1);
+            outputString[index][end - start] = '\0';
+            memmove(outputString[index], inputString + start + 1, end - start);
+            start = end;
             ++index;
         }
     }
-    return outputString;
+
+    return  outputString;
 }
 void trim(char* str1){
-    std::cout<<"sssssssssssssssssssssss";
     int temp = 0;
     int stringSize = strlen(str1);
     if(str1[0] == ' '){
